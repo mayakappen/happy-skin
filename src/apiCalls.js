4f724051f23524163b9a488dacfdc8a7838609da
Maya Kappen <mayakappen@gmail.com>
2022-09-22 17:50:25 -0600

    Add fetch for tag and product+tag

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -2,3 +2,11 @@ export const fetchByProductType = ({ type }) => {
     return fetch(`http://makeup-api.herokuapp.com/api/v1/products.json?product_type=${type}`)
         .then(response => response.json())
 }
+
+export const fetchByProductTag = ({ tag }) => {
+    return fetch(`http://makeup-api.herokuapp.com/api/v1/products?product_tags=${tag}`)
+}
+
+export const fetchByProductTypeAndTag = ({ type, tag }) => {
+    fetch(`http://makeup-api.herokuapp.com/api/v1/products?product_tags=${tag}&product_type=${type}`)
+}
