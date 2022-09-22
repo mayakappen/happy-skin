86e64d8bc710126fab4fdd1bbbcadfbbf83fef67
Maya Kappen <mayakappen@gmail.com>
2022-09-21 21:25:09 -0600

    Add router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import Navbar from './Components/Navbar'
+import {BrowserRouter as Router} from 'react-router-dom'
 
 function App() {
   return (
-    <>
+    <Router>
       <Navbar />
-    </>
+    </Router>
   );
 }
 
