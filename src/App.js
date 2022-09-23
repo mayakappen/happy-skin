a310b0d48c906c65a320da2748145f5b90d15b6e
Maya Kappen <mayakappen@gmail.com>
2022-09-22 18:10:02 -0600

    Add imports, make App a class based component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import Navbar from './Components/Navbar'
+import { Component } from 'react'
+import Category from './Components/Category/Category'
 import {BrowserRouter as Router} from 'react-router-dom'
+import {fetchByProductTag, fetchByProductType, fetchByProductTypeAndTag} from './apiCalls'
 
-function App() {
+
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      products: []
+    }
+  }
+  render() {
   return (
     <Router>
       <Navbar />
+      <Category />
     </Router>
   );
+}
 }
 
 export default App;
