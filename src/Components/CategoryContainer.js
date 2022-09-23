7a0ca1821de73361314b5c473fbfcbd5f0b415ea
Maya Kappen <mayakappen@gmail.com>
2022-09-23 16:35:55 -0600

    Add Category Container

---
+++ src/Components/CategoryContainer.js
@@ -0,0 +1,19 @@
+import Category from './Category'
+import React from 'react'
+
+const CategoryContainer = (props) => {
+    return (
+    <div className="category-container">
+      <Category className ="Hypoallergenic" id="Hypoallergenic"/>
+      <Category className="Vegan" id="Vegan"/>
+      <Category className="Cruelty-Free" id="cruelty+free"/>
+      <Category className="No-Talc" id="No+Talc"/>
+      <Category className="Alchohol-Free" id="acohol+free"/>
+      <Category className="Silicone-Free" id="silicone+free"/>
+      <Category className="Oil-Free" id="oil+free"/>
+      <Category className="Peanut-Free"/>
+    </div>  
+    );
+}
+
+export default CategoryContainer;
