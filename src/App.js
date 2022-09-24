bb9376d7c1fdc5f2ed84172392ce39fd298f6460
Maya Kappen <mayakappen@gmail.com>
2022-09-23 21:00:24 -0600

    Add conditional fetch to only get categories I don't have

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import ProductContainer from './Components/ProductContainer'
 import { Component, useEffect } from 'react'
 import CategoryContainer from './Components/Category/Category'
 import {BrowserRouter as Router} from 'react-router-dom'
-import {fetchAllProducts } from '/Users/mayakappen/turing/3module/happy-skin/src/apiCalls'
+import {fetchAllProducts, fetchByProductTag, fetchByProductType } from '/Users/mayakappen/turing/3module/happy-skin/src/apiCalls'
 
 
 
@@ -20,33 +20,35 @@ class App extends Component {
 }
 
 handleTag = event => {
-  this.setState({tag: event.target.id})
-  if (this.state.filtered === []) {
-    let filteredProducts = this.state.products.filter(product => product.tags.includes(event.target.id))
-    return this.setState({ filtered: filteredProducts })
-  }
-  else {
-    let filteredProducts = this.state.filtered.filter(product => product.tags.includes(event.target.id))
-    return this.setState({ filtered: filteredProducts })
-  }
+  fetchByProductTag(event.target.id)
+  .then(data => this.setState({ products: data }))
+  .catch(err => console.log(err))
+  // if (this.state.filtered === []) {
+  //   let filteredProducts = this.state.products
+  //   filteredProducts.filter(product => product['tag_list'].includes(event.target.id))
+  // this.setState({tag: event.target.id, filtered: filteredProducts })
+  // }
+  // else {
+  //   let filteredProducts = this.state.filtered.map(product => product.tag_list.filter(tag => tag === event.target.id))
+  //   this.setState({tag: event.target.id, filtered: filteredProducts })
+  // }
 }
 
 handleType = event => {
-  let filtered = this.state.products.filter(product => product.product_type === event.target.id)
-    this.setState({ filtered: filtered, category: event.target.id})
+  let match = this.state.products.find(product => product.product_type === event.target.id)
+  if (match !== undefined) {
+    let filtered = this.state.products.filter(product => product.product_type === event.target.id)
+    this.setState({filtered: filtered, category: event.target.id })
+  } 
+  else {
+    fetchByProductType(event.target.id)
+      .then(data => this.setState({products: this.state.products.concat(data)}))
+  }
 }
-
-componentDidMount() {
-  fetchAllProducts()
-    .then(data => this.setState({products: data }))
-    .then(error => console.log(error))  
-}
-
-
 render() {
   return (
     <Router>
-      <Navbar handler={this.handleType}/>
+      <Navbar typeHandler={this.handleType} tagHandler={this.handleTag} category={this.state.category}/>
      <ProductContainer props={this.state.category} />
       <CategoryContainer />
     </Router>
