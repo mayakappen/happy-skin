55568a0387cdeffa201a8169e0dd52617a365ff6
Maya Kappen <mayakappen@gmail.com>
2022-09-26 09:50:56 -0600

    Add index to main directory, add deployment compatible links

--- src/Components/Navbar/index.js
+++ src/Components/Navbar/index.js
@@ -22,39 +22,39 @@ render() {
             </NavLink>
             <Bars />
                 <NavMenu onClick={this.props.typeHandler}>
-                <NavLink to={"/foundation" + this.props.tag} id="Foundation" >
+                <NavLink to={"/happy-skin/foundation" + this.props.tag} id="Foundation" >
                     Foundation
                 </NavLink>
-                <NavLink to={"/blush" + this.props.tag} id="Blush">
+                    <NavLink to={"/happy-skin/blush" + this.props.tag} id="Blush">
                     Blush
                 </NavLink>
-                 <NavLink to={"/bronzer" + this.props.tag} id="Bronzer">
+                 <NavLink to={"/happy-skin/bronzer" + this.props.tag} id="Bronzer">
                     Bronzer
                  </NavLink>
-                <NavLink to={"/eyebrow" + this.props.tag} id="Eyebrow">
+                    <NavLink to={"/happy-skin/eyebrow" + this.props.tag} id="Eyebrow">
                     Eyebrow
                 </NavLink>j
-                <NavLink to={"/eyeliner" + this.props.tag} id="Eyeliner">
+                    <NavLink to={"/happy-skin/eyeliner" + this.props.tag} id="Eyeliner">
                     Eyeliner
                 </NavLink>
-                <NavLink to={"/eyeshadow" + this.props.tag} id="Eyeshadow">
+                    <NavLink to={"/happy-skin/eyeshadow" + this.props.tag} id="Eyeshadow">
                     Eyeshadow
                 </NavLink>
-                <NavLink to={"/lip_liner" + this.props.tag} id="Lip_Liner">
+                    <NavLink to={"happy-skin/lip_liner" + this.props.tag} id="Lip_Liner">
                     Lip Liner
                 </NavLink>
-                <NavLink to={"/lipstick" + this.props.tag} id="Lipstick">
+                    <NavLink to={"/happy-skin/lipstick" + this.props.tag} id="Lipstick">
                     Lipstick
                 </NavLink>
-                <NavLink to={"/mascara" + this.props.tag} id="Mascara">
+                    <NavLink to={"/happy-skin/mascara" + this.props.tag} id="Mascara">
                     Mascara
                 </NavLink>
-                <NavLink to={"/nail_polish" + this.props.tag} id="Nail_Polish">
+                    <NavLink to={"/happy-skin/nail_polish" + this.props.tag} id="Nail_Polish">
                     Nail Polish
                 </NavLink>
             </NavMenu>
             <NavBtn>
-                    <NavBtnLink to={'/favorites'}>Saved</NavBtnLink>
+                    <NavBtnLink to={'/happy-skin/favorites'}>Saved</NavBtnLink>
             </NavBtn>
         </Nav>
             
@@ -62,58 +62,58 @@ render() {
         <Tags>
               
                 <NavTags onClick={this.props.tagHandler}>
-                    <NavLink to={this.props.category + '/hypoallergenic'} id="Hypoallergenic">
+                    <NavLink to={'/happy-skin' + this.props.category + '/hypoallergenic'} id="Hypoallergenic">
                     HypoAllergenic
                 </NavLink>
-                <NavLink to={this.props.category + '/vegan'} id="Vegan">
+                <NavLink to={'/happy-skin' + this.props.category + '/vegan'} id="Vegan">
                     Vegan
                 </NavLink>
-                <NavLink to={this.props.category + '/cruelty-free'} id="cruelty+free">
+                    <NavLink to={'/happy-skin' + this.props.category + '/cruelty-free'} id="cruelty+free">
                     Cruelty-Free
                 </NavLink>
-                <NavLink to={this.props.category + '/natural'} id="Natural">
+                    <NavLink to={'/happy-skin' + this.props.category + '/natural'} id="Natural">
                     Natural
                 </NavLink>
-                <NavLink to={this.props.category + '/organic'} id="Organic">
+                    <NavLink to={'/happy-skin' + this.props.category + '/organic'} id="Organic">
                     Organic
                 </NavLink>
-                <NavLink to={this.props.category + '/ewg-verified'} id="EWG+Verified">
+                    <NavLink to={'/happy-skin' + this.props.category + '/ewg-verified'} id="EWG+Verified">
                     EWG-Verified
                 </NavLink>
-                <NavLink to={this.props.category + '/purpicks'} id="purpicks">
+                    <NavLink to={'/happy-skin' + this.props.category + '/purpicks'} id="purpicks">
                 Purpicks
                 </NavLink>
-                <NavLink to={this.props.category + '/eco-cert'} id="EcoCert">
+                    <NavLink to={'/happy-skin' + this.props.category + '/eco-cert'} id="EcoCert">
                     EcoCert
                 </NavLink>
-                <NavLink to={this.props.category + '/no-talc'} id="No+Talc">
+                    <NavLink to={'/happy-skin' + this.props.category + '/no-talc'} id="No+Talc">
                     No-Talc
                 </NavLink>
-                <NavLink to={this.props.category + '/chemical-free'} id="Chemical+Free">
+                    <NavLink to={'/happy-skin' + this.props.category + '/chemical-free'} id="Chemical+Free">
                     Chemical-Free
                 </NavLink>
-                <NavLink to={this.props.category + '/alcohol-free'} id="alcohol+free">
+                    <NavLink to={'/happy-skin' + this.props.category + '/alcohol-free'} id="alcohol+free">
                     Alcohol-Free
                 </NavLink>
-                <NavLink to={this.props.category + '/silicone-free'} id="silicone+free">
+                    <NavLink to={'/happy-skin' + this.props.category + '/silicone-free'} id="silicone+free">
                     Silicone-Free
                 </NavLink>
-                <NavLink to={this.props.category + '/oil-free'} id="oil+free">
+                    <NavLink to={'/happy-skin' + this.props.category + '/oil-free'} id="oil+free">
                     Oil-Free
                 </NavLink>
-                <NavLink to={this.props.category + '/cert-clean'} id="CertClean">
+                    <NavLink to={'/happy-skin' + this.props.category + '/cert-clean'} id="CertClean">
                     CertClean
                 </NavLink>
-                    <NavLink to={this.props.category + '/gluten-free'} id="Gluten+Free">
+                    <NavLink to={'/happy-skin' + this.props.category + '/gluten-free'} id="Gluten+Free">
                     Gluten-Free
                 </NavLink>
-                    <NavLink to={this.props.category + '/usda-organic'} id="USDA+Organic">
+                    <NavLink to={'/happy-skin' + this.props.category + '/usda-organic'} id="USDA+Organic">
                     USDA Organic
                 </NavLink>
-                <NavLink to={this.props.category + '/peanut-free-product'} id="Peanut+Free+Product">
+                    <NavLink to={'/happy-skin' + this.props.category + '/peanut-free-product'} id="Peanut+Free+Product">
                     Peanut-Free
                     </NavLink>
-                <NavLink to={this.props.category + '/water-free'} id="water+free">
+                    <NavLink to={'/happy-skin' + this.props.category + '/water-free'} id="water+free">
                     Water-Free
                 </NavLink>
 
