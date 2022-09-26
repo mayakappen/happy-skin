97e8b9dd15d0de58b03bbc1d090036e7bf8c85a3
Maya Kappen <mayakappen@gmail.com>
2022-09-26 12:11:38 -0600

    Add PropTypes

--- src/App.js
+++ src/App.js
@@ -1,73 +1,77 @@
 import Navbar from './Components/Navbar'
 import ProductContainer from './Components/ProductContainer'
-import ProductPage from './Components/ProductPage'
 import { Component} from 'react'
-import CategoryContainer from './Components/CategoryContainer'
 import {fetchAllProducts, fetchByProductTag, fetchByProductType, fetchByProductTypeAndTag } from '/Users/mayakappen/turing/3module/happy-skin/src/apiCalls'
 import {Routes, Route, useParams} from 'react-router-dom'
-import {NavBtn, NavBtnLink} from './Components/NavbarElements'
 import Favorites from './Components/Favorites'
-
+import PropTypes from'prop-types'
 
 class App extends Component {
   constructor(props) {
     super(props)
-    this.state = {
-      products: [],
-      filtered: [],
-      favorites: ['ellie'],
-      category: '',
-      currentTag: '',
-      currentProduct: {id: 0,
-                      name: '', 
-                      brand: '',
-                      price: '',
-                      rating: '',
-                      price_sign: '',
-                      api_image: '',
-                      category: '',
-                      tag_list: ''},
-      tags: [],
-      error: false,
+      this.state = {
+        products: [],
+        filtered: [],
+        favorites: [],
+        category: '',
+        currentTag: '',
+        tags: [],
+        error: false,
+        currentProduct:  
+          {
+          id: 0,
+          name: '', 
+          brand: '',
+          price: '',
+          rating: '',
+          price_sign: '',
+          api_image: '',
+          category: '',
+          tag_list: '',
+          description: '',
+        },
+        
+    }
 }
-}
+
 handleFavorite = id => {
   let fav = this.state.products.find(product => product.id === id)
   this.setState({favorites: [...this.state.favorites, fav]})
 }
+
 handleTag = event => {
-  event.preventDefault()
+event.preventDefault()
   let tag = event.target.id.split('+').join(' ')
   let category = this.state.category.substring(0)
-  console.log(category)
   let filterdd = this.state.filtered
   this.state.products.length === 0 ?
-  
-   fetchByProductTag(tag)
-    .then(data => this.setState({filtered: data, tags: [tag], currentTag: '/' + event.target.id}))
-    .catch(err => console.log(err))
+    fetchByProductTag(tag)
+      .then(data => this.setState({filtered: data, tags: [tag], currentTag: '/' + event.target.id}))
+      .catch(err => console.log(err))
   :
-  
   this.state.filtered.length === 0 ? 
-     filterdd = this.state.products.filter(product =>
-      product.tag_list.includes(event.target.id.split('+').join(' '))
-    ):
+    filterdd = this.state.products.filter(product =>
+    product.tag_list.includes(event.target.id.split('+').join(' '))
+     ):
     filterdd = this.state.filtered.filter(product => product.tag_list.includes(event.target.id.split('+').join(' ')))
   
   !this.state.tags.includes(tag) && 
     this.setState({ tags: [...this.state.tags, tag], filtered: filterdd.filter(product => product.tag_list.includes(tag)), currentTag: '/' + event.target.id, 
-      currentProduct: {id: 0,
-      name: '',
-      brand: '',
-      price: '',
-      rating: '',
-      price_sign: '',
-      api_image: '',
-      category: '',
-      tag_list: ''
-    } })
+    currentProduct: {id: 0,
+    name: '',
+    brand: '',
+    price: '',
+    rating: '',
+    price_sign: '',
+    api_image: '',
+    category: '',
+    tag_list: '',
+    description: ''
+    } 
+  })
     
-  }
+}
+
 componentDidMount() {
   fetchAllProducts()
     .then(data => this.setState({products: data}))
@@ -81,9 +85,9 @@ selectProduct = id => {
 }
 
 handleType = event => {
-event.preventDefault()
-let products = this.state.products
-let filtered = this.state.filtered
+  event.preventDefault()
+  let products = this.state.products
+  let filtered = this.state.filtered
 if (filtered.length === 0 && products.length > 0) {
   this.setState({filtered: products.filter(product => product.product_type === event.target.id.toLowerCase())})
 }
@@ -93,47 +97,48 @@ else if (filtered.length > 0 && products.length > 0 ) {
 }
 
 else if (
-filtered.length === 0 &&
-products.length === 0) 
+  filtered.length === 0 &&
+  products.length === 0) 
 {
-products = fetchByProductType(event.target.id)
-  .then(data => this.setState({
-  filtered: data
-  }))
-.catch(error => console.log(error))
+    products = fetchByProductType(event.target.id)
+    .then(data => this.setState({
+    filtered: data }))
+    .catch(error => console.log(error))
 }
 else {
   this.setState({error: true})
 }
   this.setState({
     category: `/${event.target.id}`, currentProduct: {
-      id: 0,
-      name: '',
-      brand: '',
-      price: '',
-      rating: '',
-      price_sign: '',
-      api_image: '',
-      category: '',
-      tag_list: ''
+    id: 0,
+    name: '',
+    brand: '',
+    price: '',
+    rating: '',
+    price_sign: '',
+    api_image: '',
+    category: '',
+    tag_list: '',
+    description:''
     }
   })
 }
 
-  resetFilters = () => {
-    this.setState({
-      filtered: [], tags: [], category: '', currentTag: '', currentProduct: {
-        id: 0,
-        name: '',
-        brand: '',
-        price: 0,
-        rating: 0,
-        price_sign: '',
-        api_image: '',
-        category: '',
-        tag_list: ''
-      }
-})
+resetFilters = () => {
+  this.setState({
+    filtered: [], tags: [], category: '', currentTag: '', currentProduct: {
+      id: 0,
+      name: '',
+      brand: '',
+      price: 0,
+      rating: 0,
+      price_sign: '',
+      api_image: '',
+      category: '',
+      tag_list: '',
+      description: ''
+    }
+  })
   }
   render() {
     return (
